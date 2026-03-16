f8a101fedc7ce76739d62de7b4faad3cbe9ec1d1
Pedro Lucas <pl.lucassouza@gmail.com>
2026-03-16 15:16:18 -0300

    Criando lógica para adicionar o user no banco de dados

--- users/views.py
+++ users/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 def register(request):
-  return render(request, 'register.html')
+  if request.method == 'GET':
+    return render(request, 'register.html')
+  elif request.method == 'POST':
+    print(request.POST)
+    username = request.POST.get('username')
+    senha = request.POST.get('senha')
+    confirmar_senha = request.POST.get('confirmar_senha')
+    
+    if not senha == confirmar_senha:
+      return redirect('register') 
+
+    if len(senha) < 6:
+      return redirect('register') 
+    
+    users = User.objects.filter(username=username)
+    
+    if users.exists():
+      return redirect('register')
+
+    User.objects.create_user(
+      username = username,
+      password = senha
+    )
+    
+    return redirect('login')
