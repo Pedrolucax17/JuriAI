aa2f15d36da8f8c1b893baea69fd9761e8fdac30
Pedro Lucas <pl.lucassouza@gmail.com>
2026-03-18 14:46:21 -0300

    Adicionando mensagens de erros no formulário

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +124,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+MESSAGE_TAGS = {
+    constants.SUCCESS: 'bg-green-50 text-green-700',
+    constants.ERROR: 'bg-red-50 text-red-700'
+}
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
@@ -13,14 +15,20 @@ def register(request):
     confirmar_senha = request.POST.get('confirmar_senha')
     
     if not senha == confirmar_senha:
+      print('password is equal')
+      messages.add_message(request, constants.ERROR, 'Senha e Confirmar Senha não são iguais.')
       return redirect('register') 
 
     if len(senha) < 6:
+      print('password small')
+      messages.add_message(request, constants.ERROR, 'Senha tem que ser maior do que 6 caracteres')
       return redirect('register') 
     
     users = User.objects.filter(username=username)
     
     if users.exists():
+      print('user exist')
+      messages.add_message(request, constants.ERROR, 'Usuário já existe')
       return redirect('register')
 
     User.objects.create_user(
