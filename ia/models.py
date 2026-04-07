4310b0a4a8108746c4982b37ec7afd2840877473
Pedro Lucas <pl.lucassouza@gmail.com>
2026-04-07 15:33:59 -0300

    Criando a model de Pergunta

--- ia/models.py
+++ ia/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from users.models import Cliente
 
-# Create your models here.
+class Pergunta(models.Model):
+    pergunta = models.TextField()
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.pergunta
