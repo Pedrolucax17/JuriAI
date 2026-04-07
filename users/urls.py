c4801156bf6d0dd903056d903416b1226bc41698
Pedro Lucas <pl.lucassouza@gmail.com>
2026-04-07 15:19:47 -0300

    Adicionando a URL de IA ao projeto

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -6,4 +6,5 @@ urlpatterns = [
   path('login/', views.login, name='login'),
   path('clientes/', views.clientes, name='clientes'),
   path("cliente/<int:id>", views.cliente, name='cliente'),
+  path('ia/', include('ia.urls')),
 ]
