5783de29f9c52402442693171dd8aa294b64f459
Pedro Lucas <pl.lucassouza@gmail.com>
2026-04-07 15:43:36 -0300

    Modificando a view do chat

--- ia/views.py
+++ ia/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from users.models import Cliente
+from .models import Pergunta
+from django.http import JsonResponse
 
 @csrf_exempt
 def chat(request, id):
     cliente = Cliente.objects.get(id=id)
     if request.method == 'GET':
         return render(request, 'chat.html', {'cliente': cliente})
+    elif request.method == 'POST':
+        pergunta = request.POST.get('pergunta')
+        pergunta_model = Pergunta(pergunta=pergunta, cliente=cliente)
+        pergunta_model.save()
+        return JsonResponse({'id': pergunta_model.id})
